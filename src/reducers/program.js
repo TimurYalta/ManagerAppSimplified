8048a0a51e2b760e507fdb614f19acedbe3d3bd8
Timur Yalta <timurkhalilev@gmail.com>
2019-04-02 21:54:59 +0300

    Reducers added

---
+++ src/reducers/program.js
@@ -0,0 +1,16 @@
+
+// The initial state of the App
+const initialState = {
+	id:'',
+    title:'',
+    tests:[],
+};
+
+function programReducer(state = initialState, action) {
+	switch (action.type) {
+		default:
+			return state;
+	}
+}
+
+export default programReducer;
---
+++ src/reducers/programList.js
@@ -0,0 +1,14 @@
+import {GET_TESTS} from '../constants/ActionTypes';
+
+const initialState = [];
+
+
+function programListReducer(state = initialState, action) {
+	switch (action.type) {
+        
+        default:
+			return state;
+	}
+}
+
+export default programListReducer;
